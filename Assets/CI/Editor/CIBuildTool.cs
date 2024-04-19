3e66863ac38c9ba7be20de70c51de90d9aaeffbd
Paulo Godinho <paulo@godinhosilva.com.br>
2024-04-19 02:30:08 -0300

    Changes to build unity side

--- Assets/CI/Editor/CIBuildTool.cs
+++ Assets/CI/Editor/CIBuildTool.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using JetBrains.Annotations;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEngine;
@@ -9,6 +10,60 @@ using UnityEngine;
 public class CIBuildTool
 {
     private static string[] _defineCache = {};
+
+    private static Dictionary<string, BuildTarget> PlatformDict = new Dictionary<string, BuildTarget>()
+    {
+        {"Windows", BuildTarget.StandaloneWindows64},
+        {"Mac", BuildTarget.StandaloneOSX},
+        {"Linux", BuildTarget.StandaloneLinux64}
+    };
+    
+    // External Build
+    [UsedImplicitly]
+    public static void DoBuild()
+    {
+        string selectedPlatform = "";
+        string selectedTemplate = "";
+        
+        var args = System.Environment.GetCommandLineArgs();
+        foreach (var arg in args)
+        {
+            Debug.Log("- " + arg);
+            if (arg.StartsWith("platform:"))
+            {
+                selectedPlatform = arg.Split(':')[1];
+                continue;
+            }
+
+            if (arg.StartsWith("template:"))
+            {
+                selectedTemplate = arg.Split(':')[1];
+                continue;
+            }
+        }
+        
+        Debug.Log("Selected Platform: " + selectedPlatform);
+        Debug.Log("Selected Template: " + selectedTemplate);
+
+        if (PlatformDict.ContainsKey(selectedPlatform) == false)
+        {
+            Debug.LogError("Platform not found");
+            return;
+        }
+
+        BuildTarget buildTarget = PlatformDict[selectedPlatform];
+
+        string templateToLoad = "Assets/CI/Templates/" + selectedTemplate;
+        Debug.Log(templateToLoad);
+        var template = AssetDatabase.LoadAssetAtPath<BuildTemplate>(templateToLoad);
+        if (template == null)
+        {
+            Debug.LogError("Issue loading template");
+            return;
+        }
+        
+        DoBuild(template, buildTarget);
+    }
     
     public static void DoBuild(BuildTemplate buildTemplate, BuildTarget buildTarget)
     {
@@ -54,7 +109,10 @@ public class CIBuildTool
         buildPlayerOptions.locationPathName = GetBuildTargetPath(buildTarget, buildTemplate.name);
         Debug.Log("Building to " + buildPlayerOptions.locationPathName);
         var buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        EditorUtility.RevealInFinder(buildPlayerOptions.locationPathName);
+        if (Application.isBatchMode == false)
+        {
+            EditorUtility.RevealInFinder(buildPlayerOptions.locationPathName);
+        }
     }
 
     private static string GetBuildTargetPath(BuildTarget buildTarget, string templateName)
@@ -62,19 +120,9 @@ public class CIBuildTool
         var timeStamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH-mm-ss", CultureInfo.InvariantCulture);
         var projectPath = new DirectoryInfo(Application.dataPath).Parent;
         var noSpaceProductName = Application.productName.Replace(" ", string.Empty) + ".exe";
-        var targetFolder = Path.Join(projectPath.FullName, "buildOutput/" + timeStamp + "_" + templateName + "/");
+        var targetFolder = Path.Join(projectPath.FullName, "buildOutput/" + timeStamp + "_" + templateName + "_" + buildTarget + "/");
 
         string toAppend = "";
-        switch (buildTarget)
-        {
-            case BuildTarget.StandaloneWindows:
-            case BuildTarget.StandaloneWindows64:
-                toAppend = ".exe";
-                break;
-            case BuildTarget.StandaloneOSX:
-                toAppend = ".app";
-                break;
-        }
         
         return Path.Join(targetFolder, noSpaceProductName) + toAppend;
     }
